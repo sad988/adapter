328cea9d429e6b340bd786f00d3bdfd680eb31a2
philong <philong988@gmail.com>
2018-06-12 23:27:18 +0700

    adapter connection rabbitmq

--- connect.py
+++ connect.py
@@ -1,5 +1,16 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-chanel = connection.channel()
-chanel.queue_declare('hello')
+
+class Adapter(object):
+    host = None
+    port = None
+    credentials = None
+
+    def connection(self):
+        parameters = pika.ConnectionParameters(self.host, self.port, '/', self.credentials)
+        return pika.BlockingConnection(parameters)
+
+    def channel(self):
+        connection = self.connection()
+        chanel = connection.channel()
+        chanel.queue_declare('hello')
